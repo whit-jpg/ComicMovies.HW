85a5d01f99d56b3936f8fd364a1aca5480fae10d
Willie Whitson <54363825+whit-jpg@users.noreply.github.com>
2019-10-02 14:47:21 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComicMovie.HW.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] EpicComicMovies { get; set; }
+        public void OnGet()
+        {
+            EpicComicMovies = new string[] { "Captain America: Civil War", "Doctor Strange", "Spider-Man", "Black Panther" };
+        }
+    }
+}
